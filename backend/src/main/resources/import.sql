060f3327a90a9a304ea74fb4d2eaed0ce0f05def
carlos <carlosdelarosa1009@gmail.com>
2024-11-20 15:06:59 -0500

    first commit

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO usuarios (username, password, role) VALUES ("admin", "$2a$12$yiJyceOHf8oSa/bAZH/1h.pDxP8AatIayfP5s9bcdOCs2uOoZ47xK", "ADMIN");
+
+INSERT INTO usuarios (username, password, role) VALUES  ("daniel", "$2a$12$SccxwBIF0nSDmOrcs12TbOaOUZfkah7SGU/8XywQNfY5rEhv0mpa.", "USER");
+
+
+INSERT INTO cuentas (numero_cuenta, saldo, usuario_id) VALUES (2376347, 2500.75, 1);
+
+INSERT INTO cuentas (numero_cuenta, saldo, usuario_id) VALUES (658513, 1500.50, 2);
+
+
+
+
+
+
